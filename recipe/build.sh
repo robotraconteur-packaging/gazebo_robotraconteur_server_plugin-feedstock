070dd32efc2fb053dc3ad16142063949b78f20f7
John Wason <wason@wasontech.com>
2021-05-25 00:51:45 -0400

    Set CMAKE_INSTALL_LIBDIR=lib

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,7 @@ cd gazebo_robotraconteur_server_plugin
 mkdir -p build2
 cd build2
 
-cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX -DCMAKE_PREFIX_PATH:PATH=$PREFIX -DCMAKE_BUILD_TYPE:STRING=Release ..
+cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX -DCMAKE_PREFIX_PATH:PATH=$PREFIX -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_BUILD_TYPE:STRING=Release ..
 
 cmake --build . --config Release -- -j$CPU_COUNT
 cmake --build . --config Release --target install
