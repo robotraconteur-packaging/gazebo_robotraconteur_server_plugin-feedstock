6a4f4182e65fa396e902be952867fc7cbcddd5ef
John Wason <wason@wasontech.com>
2024-08-12 15:48:59 -0400

    Use CMAKE_ARGS env var

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,10 +6,8 @@ md build2
 cd build2
 if %errorlevel% neq 0 exit /b %errorlevel%
 cmake -GNinja ^
-  -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
-  -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_BUILD_TYPE:STRING=Release ^
-  ..
+  ${CMAKE_ARGS} ..
 if %errorlevel% neq 0 exit /b %errorlevel%
 cmake --build . --config Release -j 4
 if %errorlevel% neq 0 exit /b %errorlevel%
